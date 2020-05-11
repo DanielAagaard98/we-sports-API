b8e1d3aea86417c5c852c006c9710a7c982cccde
DanielAagaard98 <daagaard18@jaumebalmes.net>
2020-05-11 19:41:46 +0200

    register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,14 +12,17 @@ class AuthController extends Controller
     public function signup(Request $request)
     {
         $request->validate([
+            'nickname' => 'required|string',
             'name' => 'required|string',
             'email' => 'required|string|email|unique:users',
-            'password' => 'required|string|confirmed',
+            'password' => 'required|string|confirmed'
         ]);
         $user = new User([
+            'nickname' => $request->nickname,
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
+
         ]);
         $user->save();
         return response()->json([
