b1404ac54411550ad9945dae28fd6c308eb3b891
DaniCristante <dcristan18@jaumebalmes.net>
2020-05-13 20:30:08 +0200

    addeed title to fillable event

--- app/Event.php
+++ app/Event.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = [
-        'creator_id', 'sport_id', 'city', 'address', 'datetime',
-        'max_participants', 'current_participants', 'img'
+        'creator_id', 'sport_id', 'title', 'city', 'address', 'datetime',
+        'max_participants', 'current_participants', 'img',
     ];
 
     public $timestamps = true;
