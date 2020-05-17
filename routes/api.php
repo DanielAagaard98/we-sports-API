26f4e893766c249bc24a19a3af5636b09da1b622
DanielAagaard98 <daagaard18@jaumebalmes.net>
2020-05-17 20:25:38 +0200

    addParticipant

--- routes/api.php
+++ routes/api.php
@@ -47,5 +47,6 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::post('events', 'EventController@createEvent');
     Route::delete('events/{id}', 'EventController@deleteEvent');
     Route::put('events/{id}', 'EventController@updateEvent');
+    Route::post('participants', 'ParticipantController@addParticipant');
 });
 
